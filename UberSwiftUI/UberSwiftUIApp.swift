1bf641757d9f3493cd8b44dd036da762f9464155
aditya10bhardwaj <138166827+aditya10bhardwaj@users.noreply.github.com>
2025-06-30 05:24:56 +0530

    Initial Commit

---
+++ UberSwiftUI/UberSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUIApp.swift
+//  UberSwiftUI
+//
+//  Created by Aditya Bhardwaj on 30/06/25.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
