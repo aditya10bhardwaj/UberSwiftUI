d427246496b17ce4f6a087bbc5bed949361fc0ad
aditya10bhardwaj <138166827+aditya10bhardwaj@users.noreply.github.com>
2025-07-02 22:08:52 +0530

    RideRequest UI and presentation logic done

--- UberSwiftUI/Core/Home/View/HomeView.swift
+++ UberSwiftUI/Core/Home/View/HomeView.swift
@@ -12,26 +12,34 @@ struct HomeView: View {
     @State private var mapState: MapViewState = .noInput
     
     var body: some View {
-        ZStack(alignment: .top) {
-            UberMapViewRepresentable(mapState: $mapState)
-                .ignoresSafeArea()
-            
-            if mapState == .noInput {
-                LocationSearchActivationView()
-                    .padding(.top, 72)
-                    .onTapGesture {
-                        withAnimation {
-                            mapState = .searchingForLocation
+        ZStack(alignment: .bottom) {
+            ZStack(alignment: .top) {
+                UberMapViewRepresentable(mapState: $mapState)
+                    .ignoresSafeArea()
+                
+                if mapState == .noInput {
+                    LocationSearchActivationView()
+                        .padding(.top, 72)
+                        .onTapGesture {
+                            withAnimation {
+                                mapState = .searchingForLocation
+                            }
                         }
-                    }
-            } else if mapState == .searchingForLocation {
-                LocationSearchView(mapState: $mapState)
+                } else if mapState == .searchingForLocation {
+                    LocationSearchView(mapState: $mapState)
+                }
+                
+                MapViewActionButton(mapState: $mapState)
+                    .padding(.leading)
+                    .padding(.top, 4)
             }
             
-            MapViewActionButton(mapState: $mapState)
-                .padding(.leading)
-                .padding(.top, 4)
+            if mapState == .locationSelected {
+                RideRequestView()
+                    .transition(.move(edge: .bottom))
+            }
         }
+        .edgesIgnoringSafeArea(.bottom)
     }
 }
 
